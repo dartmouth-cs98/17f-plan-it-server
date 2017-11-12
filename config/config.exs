76e8c2cb66643896b1d95757b253c48aa58914db
Jiyun Sung <jiyunsung@Jiyuns-MacBook-Pro.local>
2017-11-11 20:47:10 -0500

    moved the data configs to prod.exs

--- config/config.exs
+++ config/config.exs
@@ -5,10 +5,11 @@ config :plan_it,
   ecto_repos: [PlanIt.Repo]
 
 # Configure your database
-config :plan_it, PlanIt.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  database: "plan_it_dev",
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+#config :plan_it, PlanIt.Repo,
+#  adapter: Ecto.Adapters.Postgres,
+#  username: "postgres",
+#  password: "postgres",
+#  database: "postgresql-pointy-80927",
+#  url: System.get_env("DATABASE_URL"),
+#  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+#  ssl: true
