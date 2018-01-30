15dbc406e8d8b373920995f532514b5197656c47
Samuel Lee <sleeinsuk@gmail.com>
2018-01-30 12:49:14 -0500

    POC for live editing

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,14 +2,22 @@ defmodule PlanIt.RoomChannel do
   use Phoenix.Channel
 
   def join("rooms:lobby", _message, socket) do
+    {:ok, socket}
   end
 
   def join(_room, _params, _socket) do
     {:error, %{reason: "You can only join the lobby"}}
   end
 
-  def handle_in("new_message", body, socket) do
-    broadcast! socket, "new_message", body
+  def handle_in("new:msg", body, socket) do
+    IO.inspect("New message in")
+    broadcast! socket, "new:msg", body
+    {:noreply, socket}
+  end
+
+  def handle_in("new:user", body, socket) do
+    IO.inspect("New user in")
+    broadcast! socket, "new:user", body
     {:noreply, socket}
   end
 
