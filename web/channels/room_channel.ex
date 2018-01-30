cece8a5a87b8c2861ba881a3232caaeee1599cf8
Samuel Lee <sleeinsuk@gmail.com>
2018-01-30 14:28:57 -0500

    Updating repo based on ws interactions

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,5 +1,7 @@
 defmodule PlanIt.RoomChannel do
   use Phoenix.Channel
+  alias PlanIt.CardController
+  alias PlanIt.CardUtil
 
   def join("rooms:lobby", _message, socket) do
     {:ok, socket}
@@ -15,8 +17,29 @@ defmodule PlanIt.RoomChannel do
     {:noreply, socket}
   end
 
+  def handle_in("new:msg:cards", body, socket) do
+    IO.inspect("new message:cards")
+    body = Map.get(body, "body")
+    trip_id = Map.get(body, "tripId")
+    cards = Map.get(body, "cards")
+
+    #CardUtil.create_update_helper(Map.get(body, "tripId"), Map.get(body, "cards"))
+    {message, ret_package} = CardUtil.create_update_helper(trip_id, cards)
+
+    #Scrub the ret package
+    ret_package = Enum.map(ret_package, fn(c) ->
+      c
+      |> Map.drop([:__meta__, :__struct__, :trip])
+    end)
+
+    broadcast! socket, "new:msg:cards", %{cards: ret_package}
+    {:noreply, socket}
+  end
+
   def handle_in("new:user", body, socket) do
     IO.inspect("New user in")
+    IO.inspect(socket)
+    IO.inspect(body)
     broadcast! socket, "new:user", body
     {:noreply, socket}
   end
